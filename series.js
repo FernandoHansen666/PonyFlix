0a8e1f9ff75881392d743f87bb9000680bd7a6b2
Fernando Hansen <107165263+FernandoHansen666@users.noreply.github.com>
2026-07-21 15:48:00 -0300

    Add One Piece live-action to series

--- series.js
+++ series.js
@@ -20,6 +20,18 @@ window.PONYFLIX_DATA = {
       "Episódio 9": "https://serv01.meusdoramas.club/#/video/131927/1/9/",
       "Episódio 10": "https://serv01.meusdoramas.club/#/video/131927/1/10/"
     }
+  },
+  "One Piece: A Série": {
+    "Temporada 1": {
+      "Episódio 1": "https://serv01.meusdoramas.club/#/video/111110/1/1/",
+      "Episódio 2": "https://serv01.meusdoramas.club/#/video/111110/1/2/",
+      "Episódio 3": "https://serv01.meusdoramas.club/#/video/111110/1/3/",
+      "Episódio 4": "https://serv01.meusdoramas.club/#/video/111110/1/4/",
+      "Episódio 5": "https://serv01.meusdoramas.club/#/video/111110/1/5/",
+      "Episódio 6": "https://serv01.meusdoramas.club/#/video/111110/1/6/",
+      "Episódio 7": "https://serv01.meusdoramas.club/#/video/111110/1/7/",
+      "Episódio 8": "https://serv01.meusdoramas.club/#/video/111110/1/8/"
+    }
   }
 };
 
