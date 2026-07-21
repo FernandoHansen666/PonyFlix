2c60823f407161576b2824c7c153e1aacb525a76
Fernando Hansen <107165263+FernandoHansen666@users.noreply.github.com>
2026-07-21 17:29:07 -0300

    Add live-action series (Avatar, Last of Us, Fallout, Sweet Tooth)

--- series.js
+++ series.js
@@ -32,6 +32,75 @@ window.PONYFLIX_DATA = {
       "Episódio 7": "https://serv01.meusdoramas.club/#/video/111110/1/7/",
       "Episódio 8": "https://serv01.meusdoramas.club/#/video/111110/1/8/"
     }
+  },
+  "Avatar: O Último Mestre do Ar": {
+    "Temporada 1": {
+      "Episódio 1": "https://serv01.meusdoramas.club/#/video/82452/1/1/",
+      "Episódio 2": "https://serv01.meusdoramas.club/#/video/82452/1/2/",
+      "Episódio 3": "https://serv01.meusdoramas.club/#/video/82452/1/3/",
+      "Episódio 4": "https://serv01.meusdoramas.club/#/video/82452/1/4/",
+      "Episódio 5": "https://serv01.meusdoramas.club/#/video/82452/1/5/",
+      "Episódio 6": "https://serv01.meusdoramas.club/#/video/82452/1/6/",
+      "Episódio 7": "https://serv01.meusdoramas.club/#/video/82452/1/7/",
+      "Episódio 8": "https://serv01.meusdoramas.club/#/video/82452/1/8/"
+    }
+  },
+  "The Last of Us": {
+    "Temporada 1": {
+      "Episódio 1": "https://serv01.meusdoramas.club/#/video/100088/1/1/",
+      "Episódio 2": "https://serv01.meusdoramas.club/#/video/100088/1/2/",
+      "Episódio 3": "https://serv01.meusdoramas.club/#/video/100088/1/3/",
+      "Episódio 4": "https://serv01.meusdoramas.club/#/video/100088/1/4/",
+      "Episódio 5": "https://serv01.meusdoramas.club/#/video/100088/1/5/",
+      "Episódio 6": "https://serv01.meusdoramas.club/#/video/100088/1/6/",
+      "Episódio 7": "https://serv01.meusdoramas.club/#/video/100088/1/7/",
+      "Episódio 8": "https://serv01.meusdoramas.club/#/video/100088/1/8/",
+      "Episódio 9": "https://serv01.meusdoramas.club/#/video/100088/1/9/"
+    }
+  },
+  "Fallout": {
+    "Temporada 1": {
+      "Episódio 1": "https://serv01.meusdoramas.club/#/video/106379/1/1/",
+      "Episódio 2": "https://serv01.meusdoramas.club/#/video/106379/1/2/",
+      "Episódio 3": "https://serv01.meusdoramas.club/#/video/106379/1/3/",
+      "Episódio 4": "https://serv01.meusdoramas.club/#/video/106379/1/4/",
+      "Episódio 5": "https://serv01.meusdoramas.club/#/video/106379/1/5/",
+      "Episódio 6": "https://serv01.meusdoramas.club/#/video/106379/1/6/",
+      "Episódio 7": "https://serv01.meusdoramas.club/#/video/106379/1/7/",
+      "Episódio 8": "https://serv01.meusdoramas.club/#/video/106379/1/8/"
+    }
+  },
+  "Sweet Tooth": {
+    "Temporada 1": {
+      "Episódio 1": "https://serv01.meusdoramas.club/#/video/103768/1/1/",
+      "Episódio 2": "https://serv01.meusdoramas.club/#/video/103768/1/2/",
+      "Episódio 3": "https://serv01.meusdoramas.club/#/video/103768/1/3/",
+      "Episódio 4": "https://serv01.meusdoramas.club/#/video/103768/1/4/",
+      "Episódio 5": "https://serv01.meusdoramas.club/#/video/103768/1/5/",
+      "Episódio 6": "https://serv01.meusdoramas.club/#/video/103768/1/6/",
+      "Episódio 7": "https://serv01.meusdoramas.club/#/video/103768/1/7/",
+      "Episódio 8": "https://serv01.meusdoramas.club/#/video/103768/1/8/"
+    },
+    "Temporada 2": {
+      "Episódio 1": "https://serv01.meusdoramas.club/#/video/103768/2/1/",
+      "Episódio 2": "https://serv01.meusdoramas.club/#/video/103768/2/2/",
+      "Episódio 3": "https://serv01.meusdoramas.club/#/video/103768/2/3/",
+      "Episódio 4": "https://serv01.meusdoramas.club/#/video/103768/2/4/",
+      "Episódio 5": "https://serv01.meusdoramas.club/#/video/103768/2/5/",
+      "Episódio 6": "https://serv01.meusdoramas.club/#/video/103768/2/6/",
+      "Episódio 7": "https://serv01.meusdoramas.club/#/video/103768/2/7/",
+      "Episódio 8": "https://serv01.meusdoramas.club/#/video/103768/2/8/"
+    },
+    "Temporada 3": {
+      "Episódio 1": "https://serv01.meusdoramas.club/#/video/103768/3/1/",
+      "Episódio 2": "https://serv01.meusdoramas.club/#/video/103768/3/2/",
+      "Episódio 3": "https://serv01.meusdoramas.club/#/video/103768/3/3/",
+      "Episódio 4": "https://serv01.meusdoramas.club/#/video/103768/3/4/",
+      "Episódio 5": "https://serv01.meusdoramas.club/#/video/103768/3/5/",
+      "Episódio 6": "https://serv01.meusdoramas.club/#/video/103768/3/6/",
+      "Episódio 7": "https://serv01.meusdoramas.club/#/video/103768/3/7/",
+      "Episódio 8": "https://serv01.meusdoramas.club/#/video/103768/3/8/"
+    }
   }
 };
 
